97cd63a2dc477ad4cb652507b1e0d76b561d648a
TheDan <50120073+dbathaei@users.noreply.github.com>
2024-05-15 19:20:24 -0700

    regular changes

--- BaseObjects.py
+++ BaseObjects.py
@@ -16,6 +16,7 @@ class Card:
         "Joker": 14
     }
 
+    # Attributes:
     def __init__(self, face, suit):
         self._face = face
         self._suit = suit
@@ -37,12 +38,15 @@ class Card:
     # setters
     #-------------------
 
+    # set the unique ID for the card.
     def set_ID(self, ID):
         self._unique_ID = ID
     
+    # set the value of the card off a dictionary.
     def set_value(self, valueDict: dict):
         self._value = valueDict[self._face]
     
+    # set the value of a card directly to an integer.
     def set_value_direct(self, value: int):
         self._value = value
     
@@ -67,8 +71,16 @@ class Card:
 
     def get_value(self) -> int:
         return int(self.GEN_INDEX[self._face])
+    
+    """
+        As you can see, cards are very simple, and relatively static.
+        They are passed around, but almost never modified.
+    """
+
 
 class Player:
+
+    # Attributes:
     def __init__(self, name, money = 0, DOB: int = 00000000, unique_ID = None):
         self._name = name
         self._money = money
@@ -87,15 +99,27 @@ class Player:
     #-------------------
     # setters
     #-------------------
+
+    # adds a hand to the player.
     def set_hand(self, hand):
         self._hands.append(hand)
     
+    # removes a hand from the player.
     def remove_hand(self, hand):
         self._hands.remove(hand)
 
+    # sets the unique ID for the player.
     def set_ID(self, ID):
         self._unique_ID = ID
     
+    '''
+        the `set_strategy` function sets the betting and playing strategy
+        for the player. These strategies are not at all defined!!!
+        You have to come up with them given the game you are making.
+        
+        As such, they are designed to be flexible. They could be
+        functions, class methods, or even connected to user input.
+    '''
     def set_strategy(self, strategy, type):
         if type == "bet":
             self._betting_strategy = strategy
@@ -115,9 +139,11 @@ class Player:
     def DOB(self) -> int:
         return self._DOB
     
+    # returns a player's bet decision given their betting strategy.
     def bet_strategy(self, *args, **kwargs):
         return self._betting_strategy(*args, **kwargs)
 
+    # returns a player's action decision given their playing strategy.
     def player_strategy(self, *args, **kwargs):
         return self._playing_strategy(*args, **kwargs)
     
@@ -125,19 +151,25 @@ class Player:
     # cash mechanics
     #-------------------
 
+    # adds chips to the player's money.
+    # this should be used when players buys chips.
     def get_chips(self, amount: float):
         self._money += amount
 
+    # puts chips into the player's hand.
     def buys_in(self, amount: float, hand):
         self._money -= amount
         hand._total_cash += amount
     
+    # takes chips off player's hand and brings them back to player's pocket.
     def cashes_out(self, hand):
         self._money += hand._total_cash
         hand._total_cash = 0
 
 
 class Hand:
+
+    # Attributes:
     def __init__(self, player = None, unique_ID = None):
         self._cards = []
         self._values = []
@@ -166,21 +198,22 @@ class Hand:
     # setters
     #-------------------
 
+    # sets the unique ID for the hand.
     def set_ID(self, ID):
         self._unique_ID = ID
     
+    # defines a player for the hand.
     def set_player(self, player = None):
         if self._player:
             if self._player != player:
                 self._player.remove_hand(self)
 
-        if player == None:
-            self._player = player
-        else:
-            self._player = player
+        self._player = player
+        if player is not None:
             player.set_hand(self)
 
 
+    # clears hand and all the attributes that were changed.
     def reset_hand(self):
         self._cards = []
         self._values = []
@@ -192,12 +225,15 @@ class Hand:
         self._extra = False
         self._action_number = 1
 
+    # sets a special attribute for the hand.
+    # you can add as many special attributes as you want.
     def set_sa(self, attribute: str, someValue = True):
         self._special_attributes[attribute] = someValue   
     
     #-------------------
     # getters
     #-------------------
+    
     def get_action_number(self) -> int:
         return self._action_number
     
--- DeckGen.py
+++ DeckGen.py
@@ -3,26 +3,39 @@ from IDgenerator import GEN_HASH_CARD
 from random import shuffle
 
 def DECK_GENERATOR(nDecks: int = 1, jokers: bool = False, aValueDict = None):
+
+    # deck to be returned
     deck = []
+
+    # faces and suits
     faces = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
     suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
+    # creates `nDecks` decks.
     for deck_number in range(1, nDecks+1):
+
+        # iterates through suits and faces.
         for suit in suits:
             for face in faces:
+
+                # generates a unique ID for each card.
                 hash_id = GEN_HASH_CARD(face, suit, deck_number)
+
+                # instatiates a card object.
                 thisCard = Card(face, suit)
                 if aValueDict:
                     thisCard.set_value(aValueDict)
+                
+                # sets the unique ID for the card.
                 thisCard.set_ID(hash_id)
                 deck.append(thisCard)
 
+        # if jokers are to be included, add them to the deck.
         if jokers == True:
-            for _ in range(2):
-                if _ == 0:
-                    jsuit = "Red"
-                else:
-                    jsuit = "Black"
-                hash_id = GEN_HASH_CARD("Joker", jsuit, deck_number)
-                deck.append(Card("Joker", jsuit, aValueDict))
+            hash_id = GEN_HASH_CARD("Joker", "Red", deck_number)
+            deck.append(Card("Joker", "Red", aValueDict))
+            hash_id = GEN_HASH_CARD("Joker", "Black", deck_number)
+            deck.append(Card("Joker", "Black", aValueDict))
+    
     return deck
+
